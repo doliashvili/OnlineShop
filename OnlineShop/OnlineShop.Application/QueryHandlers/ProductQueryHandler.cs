45499306b63ea712e01c355cfc657b1f4dc19d02
ლევან დოლიაშვილი <doliashvili.levan@itstep.ge>
2021-04-13 17:25:56 +0400

    add ProductQueryHandlers

--- OnlineShop/OnlineShop.Application/QueryHandlers/ProductQueryHandler.cs
+++ OnlineShop/OnlineShop.Application/QueryHandlers/ProductQueryHandler.cs
@@ -8,7 +8,10 @@ using OnlineShop.Domain.Products.ReadModels;
 namespace OnlineShop.Application.QueryHandlers
 {
     public sealed class ProductQueryHandler : IQueryHandler<GetAllProducts, List<ProductReadModel>>,
-        IQueryHandler<GetAllProductCount,int>
+        IQueryHandler<GetAllProductCount, int>,
+        IQueryHandler<GetProducts, PagingProductModel>,
+        IQueryHandler<GetFilteredProducts, PagingProductModel>,
+        IQueryHandler<GetProductById, ProductReadModel>
     {
         private readonly IProductReadRepository _readRepository;
 
@@ -18,12 +21,27 @@ namespace OnlineShop.Application.QueryHandlers
         }
         public Task<List<ProductReadModel>> HandleAsync(GetAllProducts query)
         {
-           return _readRepository.GetAllProductAsync(query);
+            return _readRepository.GetAllProductAsync(query);
         }
 
         public Task<int> HandleAsync(GetAllProductCount query)
         {
             return _readRepository.GetAllProductCountAsync(query);
+        }
+
+        public Task<PagingProductModel> HandleAsync(GetProducts query)
+        {
+            return _readRepository.GetProductsAsync(query);
+        }
+
+        public Task<PagingProductModel> HandleAsync(GetFilteredProducts query)
+        {
+            return _readRepository.GetFilteredProductsAsync(query);
+        }
+
+        public Task<ProductReadModel> HandleAsync(GetProductById query)
+        {
+            return _readRepository.GetProductByIdAsync(query);
         }
     }
 }
