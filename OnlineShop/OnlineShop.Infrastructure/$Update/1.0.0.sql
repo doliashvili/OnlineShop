1072460e6febe1d02f792f73b74ce3c780fbea73
ლევან დოლიაშვილი <doliashvili.levan@itstep.ge>
2021-04-14 14:42:08 +0400

    MC

--- OnlineShop/OnlineShop.Infrastructure/$Update/1.0.0.sql
+++ OnlineShop/OnlineShop.Infrastructure/$Update/1.0.0.sql
@@ -5,30 +5,30 @@ GO
 
 -- dbo
 
---CREATE TABLE [dbo].[SysSettings](
---	[Id] [varchar](50) NOT NULL,
---	[Value] [nvarchar](255) NOT NULL,
---	CONSTRAINT [PK_dbo_SysSettings] PRIMARY KEY CLUSTERED ([Id])
---	WITH (FILLFACTOR = 100)
---) 
---GO
+CREATE TABLE [dbo].[SysSettings](
+	[Id] [varchar](50) NOT NULL,
+	[Value] [nvarchar](255) NOT NULL,
+	CONSTRAINT [PK_dbo_SysSettings] PRIMARY KEY CLUSTERED ([Id])
+	WITH (FILLFACTOR = 100)
+) 
+GO
 
---CREATE PROCEDURE [dbo].[SetSysSetting] (@id varchar(50), @value nvarchar(255))
---AS
---BEGIN
---	SET NOCOUNT ON;
+CREATE PROCEDURE [dbo].[SetSysSetting] (@id varchar(50), @value nvarchar(255))
+AS
+BEGIN
+	SET NOCOUNT ON;
 
---	UPDATE [dbo].[SysSettings] WITH (ROWLOCK)
---	SET [Value] = @value
---	WHERE [Id] = @id;
---	IF @@ROWCOUNT = 0
---	BEGIN
---		INSERT INTO [dbo].[SysSettings] WITH (ROWLOCK) ([Id], [Value])
---		VALUES (@id, @value);
---	END;
---	RETURN 0;
---END;
---GO
+	UPDATE [dbo].[SysSettings] WITH (ROWLOCK)
+	SET [Value] = @value
+	WHERE [Id] = @id;
+	IF @@ROWCOUNT = 0
+	BEGIN
+		INSERT INTO [dbo].[SysSettings] WITH (ROWLOCK) ([Id], [Value])
+		VALUES (@id, @value);
+	END;
+	RETURN 0;
+END;
+GO
 
 -- OnlineShop
 
