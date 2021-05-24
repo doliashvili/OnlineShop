a5b1e598aa25d954d0fa14597f43fa76d45f35a1
ლევან დოლიაშვილი <doliashvili.levan@itstep.ge>
2021-05-24 19:14:00 +0400

    Implement CartService

--- OnlineShop/OnlineShop.UI/Services/Abstract/ICartService.cs
+++ OnlineShop/OnlineShop.UI/Services/Abstract/ICartService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using OnlineShop.UI.Models.Cart;
 
@@ -8,10 +9,12 @@ namespace OnlineShop.UI.Services.Abstract
 {
     public interface ICartService
     {
-        Task<string> AddCartAsync(AddCartRequest addCartRequest);
+        Task<string> AddCartAsync(AddCartRequest addCartRequest, string token, CancellationToken cancellationToken);
 
-        Task<string> DeleteCartAsync(long cartId);
+        Task<string> DeleteCartAsync(long cartId, string token, CancellationToken cancellationToken);
 
-        Task<List<CartViewModel>> GetCartsAsync(string userId);
+        Task<string> UpdateCartAsync(UpdateCartRequest updateCartRequest, string token, CancellationToken cancellationToken);
+
+        Task<List<CartViewModel>> GetCartsAsync(string userId, string token, CancellationToken cancellationToken);
     }
 }
--- OnlineShop/OnlineShop.UI/Services/Implements/CartService.cs
+++ OnlineShop/OnlineShop.UI/Services/Implements/CartService.cs
@@ -1,27 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using OnlineShop.UI.Extensions;
+using OnlineShop.UI.Helpers;
 using OnlineShop.UI.Models.Cart;
+using OnlineShop.UI.Models.Product;
 using OnlineShop.UI.Services.Abstract;
 
 namespace OnlineShop.UI.Services.Implements
 {
     public class CartService : ICartService
     {
-        public Task<string> AddCartAsync(AddCartRequest addCartRequest)
+        private readonly HttpClient _httpClient;
+
+        public CartService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
         }
 
-        public Task<string> DeleteCartAsync(long cartId)
+        public async Task<string> AddCartAsync(AddCartRequest addCartRequest, string token, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Add("Bearer", token);
+
+            var response = await _httpClient.SendAsync<string>(HttpMethod.Post, "v1/Cart/AddCart", addCartRequest.AsJson(), cancellationToken);
+            return response;
         }
 
-        public Task<List<CartViewModel>> GetCartsAsync(string userId)
+        public async Task<string> DeleteCartAsync(long cartId, string token, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _httpClient.DefaultRequestHeaders.Add("Bearer", token);
+
+            var response = await _httpClient.SendAsync<string>(HttpMethod.Delete, "v1/Cart/DeleteCart", new { Id = cartId }.AsJson(), cancellationToken);
+            return response;
+        }
+
+        public async Task<string> UpdateCartAsync(UpdateCartRequest updateCartRequest, string token, CancellationToken cancellationToken)
+        {
+            _httpClient.DefaultRequestHeaders.Add("Bearer", token);
+
+            var response = await _httpClient.SendAsync<string>(HttpMethod.Post, "v1/Cart/GetAllCarts", updateCartRequest.AsJson(), cancellationToken);
+            return response;
+        }
+
+        public async Task<List<CartViewModel>> GetCartsAsync(string userId, string token, CancellationToken cancellationToken)
+        {
+            _httpClient.DefaultRequestHeaders.Add("Bearer", token);
+
+            var response = await _httpClient.SendAsync<List<CartViewModel>>(HttpMethod.Get, "v1/Cart/GetAllCarts", new { Id = userId }.AsJson(), cancellationToken);
+            return response;
         }
     }
 }
